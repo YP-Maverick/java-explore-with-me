7a6f93b3786cc762512edd1da1f72fc00d85f2fc
EgorIvanov <Egor.Ivanov.v@yandex.ru>
2024-09-24 18:52:35 +0300

    feat: StatsClient

---
+++ ewm-main/src/main/java/ru/pacticum/ewm/main/client/StatsClient.java
@@ -0,0 +1,25 @@
+package ru.pacticum.ewm.main.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import ru.practicum.ewm.stats.dto.dto.CreateStatsDto;
+import ru.practicum.ewm.stats.dto.dto.StatsDto;
+
+
+import java.util.List;
+
+@FeignClient(value = "stats-client", url = "${statsclient.url}")
+public interface StatsClient {
+
+    @PostMapping("/hit")
+    CreateStatsDto createStats(@RequestBody CreateStatsDto creationDto);
+
+    @GetMapping("/stats?start={start}&end={end}&uris={uris}&unique={unique}")
+    List<StatsDto> getStats(@RequestParam String start,
+                            @RequestParam String end,
+                            @RequestParam(required = false) String[] uris,
+                            @RequestParam(defaultValue = "false") boolean unique);
+}
