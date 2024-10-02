7aee978de3ff0b338f2bbcb9c9e19c3d2c48a012
EgorIvanov <Egor.Ivanov.v@yandex.ru>
2024-10-03 02:44:06 +0300

    feat: HTTP Client

--- stats/stats-service/src/main/java/ru/practicum/ewm/stats/service/configuration/StatsServiceConfig.java
+++ stats/stats-service/src/main/java/ru/practicum/ewm/stats/service/configuration/StatsServiceConfig.java
@@ -1,8 +1,8 @@
 package ru.practicum.ewm.stats.service.configuration;
 
 import org.springframework.context.annotation.Configuration;
-import ru.practicum.ewm.stats.dto.config.StatsConfig;
+import ru.practicum.ewm.stats.dto.config.DataTimeStatsConfig;
 
 @Configuration
-public class StatsServiceConfig extends StatsConfig {
+public class StatsServiceConfig extends DataTimeStatsConfig {
 }
